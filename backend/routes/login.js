22e66dc1b4592a8931ca9569abfcdcebde9da2d0
Stephen491 <60985179+Stephen491@users.noreply.github.com>
2020-09-17 16:26:27 -0400

    Update to login and register

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,34 +1,22 @@
 var express = require('express');
 var router = express.Router();
-var mysql = require('mysql');
+var cassandra = require('cassandra-driver');
 const argon2 = require('argon2');
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
 
-var connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
-  })
-
-  connection.connect(function (err) {
-    if (err) {
-        console.log('ERROR:', err.code);
-        
-    } else {
-        console.log('Connected to DB')
-    }
-});
-
+var connection = new cassandra.Client({
+    contactPoints: [process.env.CASSANDRA_DB_HOST],
+    localDataCenter: process.env.CASSANDRA_DB_DATACENTER,
+    keyspace: process.env.CASSANDRA_DB_KEYSPACE
+  });
 
 
 router.get('/', function(req, res, next) {
     res.send('API is working properly');
 });
 
-router.post('/', function(req, res, next) { 
+router.post('/', async function(req, res, next) { 
     let emailAddress = req.body.email;
     let password = req.body.password; 
     let requestGranted = false;
@@ -38,65 +26,54 @@ router.post('/', function(req, res, next) {
     let user;
 
     console.log("Login attempt received");
-    seekAccount();
+  
+    let query = "SELECT id, passwordhash FROM user_accounts_login_credentials WHERE email = ?"; 
+    connection.execute(query, [req.body.email]).then(async function(result) {
+        if(!result.rows[0]){
+            notFound = true;
+            response = {err: true, notFound: true, wrongPassword: wrongPassword, token: null};                
+            res.json(response); 
+        }
+        else {
+            
+            try {
+                console.log(result.rows[0]);
+               let dbPasswordHash = result.rows[0].passwordhash; 
+               if(await argon2.verify(dbPasswordHash, password)) {
+                   //correct password 
+                   console.log("correct password");
+                    user = {
+                       userid: result.rows[0].id,
+                       username: result.rows[0].username,
+                       email: emailAddress,
+                   }
+                   res.cookie("jid", jwt.sign({user: user}, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '7d' }), {httpOnly: true});
+                   jwt.sign({user: user}, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15m' }, (err, token) => {
+                          res.json({err: false, notFound: false, wrongPassword: false, accessToken: token});
+                    });
+
+
+                }//end if correct password
+                else{
+                     //incorrect password
+                    wrongPassword = true;
+                    console.log("incorrect password");
+                    response = {err: true, notFound: false, wrongPassword: wrongPassword, token: null};
+                    res.json(response);
+                }// end else incorrect password
+
+            }
+            catch(err){
+                //hashing error
+                throw err;
+            }
+
+
+        }
+
+    });//end query command
+
     
-    function seekAccount() {
-
-        let queryCommand = "";
-        queryCommand = queryCommand.concat("SELECT userid, username, passwordhash FROM useraccounts WHERE email = \'", emailAddress, "\';"); 
-        connection.query(queryCommand, async function(err, rows, field) {
-            if(rows.length==0){
-                //if user for given email doesn't exist
-                notFound = true;
-                response = {err: true, notFound: true, wrongPassword: wrongPassword, token: null};
-                res.json(response); 
-            }
-            else {
-                //if user for given email exists
-                dbPasswordHash = rows[0].passwordhash;
-                try {
-                   if(await argon2.verify(dbPasswordHash, password)) {
-                       //correct password
-                       console.log("correct password");
-                       user = {
-                           userid: rows[0].userid,
-                           username: rows[0].username,
-                           email: emailAddress,
-                       }
-                       res.cookie("jid", jwt.sign({user: user}, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '7d' }), {httpOnly: true});
-
-                       jwt.sign({user: user}, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15m' }, (err, token) => {
-                            res.json({err: false, notFound: false, wrongPassword: false, accessToken: token});
-                       });
-
-
-                   }//end if correct password
-                   else{
-                       //incorrect password
-                       wrongPassword = true;
-                       console.log("incorrect password");
-                       response = {err: true, notFound: false, wrongPassword: wrongPassword, token: null};
-                       res.json(response);
-                   }// end else incorrect password
-
-                }
-                catch(err){
-                    //hashing error
-                    throw err;
-                }
-
-
-            }
-
-        })//end query command
-
-    }//end seekAccount
-
-    response = {
-        userid: '',
-        username: '',
-        accessGranted: requestGranted,
-    }
 });
 
 module.exports = router;
--- backend/routes/register.js
+++ backend/routes/register.js
@@ -24,7 +24,7 @@ router.post('/', async function(req, res, next) {
     let emailTaken = false;
     let usernameTaken = false;
     let email = req.body.email;
-   
+    let response; 
 
     let findUsernameCommand = 'SELECT * FROM user_accounts_username WHERE username = ?';
 
@@ -33,80 +33,78 @@ router.post('/', async function(req, res, next) {
     
     let insertCommand; 
     
-    await connection.execute(findUsernameCommand, [req.body.username]).then(result => 
-        usernameTaken = result.rows[0]
-        );
-
-    await connection.execute(findEmailCommand, [req.body.email]).then(result => 
-         emailTaken = result.rows[0]
-        );
-
-
-
-
-    if(!emailTaken&&!usernameTaken) {
-        console.log("credentials are OK");
-
-        //TODO: Insert data and response to client
-
-
-
-
-
-    }
-
-        /** 
-    if(!usernameOrEmailTaken) {
-        console.log("ranconditional");
-        insertData();
-    }
-
-    async function insertData() {
-        try {
-            const passwordHash = await argon2.hash(password);
-            let insertCommand = 'INSERT';
-          
-            insertCommand = insertCommand.concat(' INTO useraccounts (email, username, passwordhash, dob) VALUES (\'',req.body.email,'\', \'',req.body.username,'\', \'',passwordHash, '\', STR_TO_DATE(DATE_FORMAT(\'',req.body.dob.split('T')[0],'\', \'%Y %M %D\'),\'%Y %M %D\'))');
-
-            connection.query(insertCommand, function(err, rows, field){
-                if(err){
-                 if(err.errno==1062) {
-                    let errorMessage = err.sqlMessage.split(' ');
-                    if(errorMessage[errorMessage.length-1]==="'useraccounts.email'") {                  
-                            hasError1 = true;
-                            emailTaken1 = true; 
-                    }
-                    else if(errorMessage[errorMessage.length-1]==="'useraccounts.username'") { 
-                            hasError1 = true;
-                            usernameTaken1 = true;
-                    }
-                }
-                 else {
-                    throw error;
-                    }
-                }
-                console.log(emailTaken1);   
-                let response = {
-                    hasError: hasError1,
-                    emailTaken: emailTaken1,
-                    usernameTaken: usernameTaken1,
-        
-                };
-                res.type('json');
-                res.send(response);
-
-            }//end query callback
-            );//end query
-
-            connection.release();
-        }//end try
-        catch(err){
+    await connection.execute(findUsernameCommand, [req.body.username]).then(function(result) {
+            if(result.rows[0]) {
+                usernameTaken = true;
+                hasError = true;
+            }
+        }).catch(function(err) {
             hasError = true;
-            errorType = "Password hashing Error";
-        
-        }//end catch
-    }//insert data function
-    */
+            console.log(err);
+        });
+
+    await connection.execute(findEmailCommand, [req.body.email]).then(function(result) {
+            if(result.rows[0]) {
+                emailTaken = true;
+                hasError = true;
+            }
+        }
+        ).catch(function(err){
+            hasError = true;
+            console.log(err);
+
+        });
+
+    if(!emailTaken&&!usernameTaken&&!hasError) {
+        console.log("credentials are OK");
+        const passwordHash = await argon2.hash(password);
+        let formattedDOB = req.body.dob.split('T')[0]
+        let insertIntoCredentialsByID = 'INSERT INTO user_accounts_credentials(id, email, passwordhash) VALUES (now(), ?, ?)'
+        let insertIntoCredentialsByEmail = 'INSERT INTO user_accounts_login_credentials(email, passwordhash, id, username) VALUES (?,?, now(), ?)'
+        let insertIntoIdByEmail = 'INSERT INTO user_accounts_email(email, id) VALUES (?, now())'
+        let insertIntoIdByUsername = 'INSERT INTO user_accounts_username(username, id) VALUES (?, now())'
+        let insertIntoUseraccountsInfoById = 'INSERT INTO user_accounts_info(id, email, username, dob) VALUES (now(), ?, ?, ?)'
+        let queries = [
+            {query: insertIntoCredentialsByID, params: [req.body.email, passwordHash]},
+            {query: insertIntoCredentialsByEmail, params: [req.body.email, passwordHash, req.body.username]},
+            {query: insertIntoIdByEmail, params: [req.body.email]},
+            {query: insertIntoIdByUsername, params: [req.body.username]},
+            {query: insertIntoUseraccountsInfoById, params: [req.body.email, req.body.username, formattedDOB]}
+        ]
+
+
+        connection.batch(queries, {prepare: true}).then(function() {
+            response = {
+                hasError: hasError,
+                emailTaken: emailTaken,
+                usernameTaken: usernameTaken
+            }
+
+
+            console.log("inserted");
+            res.type('json');
+            res.send(response);
+
+        }).catch(function(err) {
+            console.log(err);
+            hasError = true;
+        });
+
+    }
+    else {
+        console.log("Duplicate email/username")
+        response = {
+            hasError: hasError,
+            emailTaken: emailTaken,
+            usernameTaken: usernameTaken
+        }
+        res.type('json');
+        res.send(response);
+
+
+
+    }
+
 });//end post
 
 module.exports = router;
